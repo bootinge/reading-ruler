15c771393ff39581ba6452ab5cd914d78775cb18
Denison Linus <dlmtavar@gmail.com>
2014-11-12 19:06:26 -0200

    loop optimization

--- debug/highlighter.js
+++ debug/highlighter.js
@@ -78,7 +78,7 @@ var highlighter = {
                 char = 0
             ;
             if (str.length == 0) return hash;
-            for (i = 0; i < str.length; i++) {
+            for (var i = 0, j = str.length; i < j; i++) {
                 char = str.charCodeAt(i);
                 hash = ((hash<<5)-hash)+char;
                 hash = hash & hash; // Convert to 32bit integer
